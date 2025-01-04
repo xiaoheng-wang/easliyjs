d27876dc6de0b8657c7589808c95b4e6f26876b8
肖恒 <wangliusuihenliu@gmail.com>
2025-01-04 11:11:35 +0800

    feat(验证): :sparkles: 23

--- play/src/App.vue
+++ play/src/App.vue
@@ -3,15 +3,17 @@ import { ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 const re = ref('')
+
+const a = 1
 </script>
 
 <template>
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
